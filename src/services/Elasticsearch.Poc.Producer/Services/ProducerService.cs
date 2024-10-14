940b5b0665963bfb6c5137663327c196d93adb00
Kubing Lin <edkt973@gmail.com>
2024-10-14 16:14:56 +0800

    fix: 實作 ProducerService

--- src/services/Elasticsearch.Poc.Producer/Services/ProducerService.cs
+++ src/services/Elasticsearch.Poc.Producer/Services/ProducerService.cs
@@ -1,9 +1,28 @@
-﻿namespace Elasticsearch.Poc.Producer.Services
+﻿using Elasticsearch.Poc.Helpers;
+using Microsoft.Extensions.Logging;
+
+namespace Elasticsearch.Poc.Producer.Services
 {
     public class ProducerService : IProducerService
     {
+        private readonly IElasticsearchHelper<DefaultElasticsearchOptions> _elasticsearchHelper;
+        private readonly ILogger<ProducerService> _logger;
+
+        public ProducerService(
+            IElasticsearchHelper<DefaultElasticsearchOptions> elasticsearchHelper,
+            ILogger<ProducerService> logger)
+        {
+            _elasticsearchHelper = elasticsearchHelper;
+            _logger = logger;
+        }
+
         public async Task PublishMessagesAsync(CancellationToken cancellationToken = default)
         {
+            var cluster = _elasticsearchHelper.GetCluster();
+            var ping = await cluster.PingOperations.PingAsync();
+
+            _logger.LogInformation("Ping: {Ping}", ping);
+
             await Task.Delay(300, cancellationToken);
         }
     }
